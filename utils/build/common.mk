b12458db1a375173d16fdc571a40639f5a118ec8
Duzy Chan <code@duzy.info>
2015-12-08 16:06:47 +0800

    update common.mk

--- utils/build/common.mk
+++ utils/build/common.mk
@@ -1,11 +1,12 @@
-LLVM_ROOT := $(or $(wildcard /open/llvm),$(wildcard ~/Tools/llvm))
+LLVM_ROOT := $(wildcard /open/llvm)
 LLVM := $(LLVM_ROOT)/build
-LLVM_CONFIG := $(LLVM)/Debug+Asserts/bin/llvm-config
-LLVM_DIS := $(LLVM)/Debug+Asserts/bin/llvm-dis
-LLVMTableGen := $(LLVM)/Debug+Asserts/bin/llvm-tblgen -I$(LLVM_ROOT)/include
-LLI := $(LLVM)/Debug+Asserts/bin/lli
+LLVM_BUILD := $(LLVM_ROOT)/build
+LLVM_CONFIG := $(LLVM_BUILD)/bin/llvm-config
+LLVM_DIS := $(LLVM_BUILD)/bin/llvm-dis
+LLVMTableGen := $(LLVM_BUILD)/bin/llvm-tblgen -I$(LLVM_ROOT)/include
+LLI := $(LLVM_BUILD)/bin/lli
 
-BOOST_ROOT := $(or $(wildcard /open/boost_1_58_0),$(wildcard ~/Tools/boost_1_58_0))
+BOOST_ROOT := $(wildcard /open/boost_1_59_0)
 
 CXXSTD := -std=c++11 -std=c++1y
 
